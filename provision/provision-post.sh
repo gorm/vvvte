111ff3662a145207bca25fe0c9d944afe8ed0e4e
gorm <goer@dss.dep.no>
2013-11-12 07:52:14 +0100

    copy in plugins, mu-plugins and themes from main network

--- provision/provision-post.sh
+++ provision/provision-post.sh
@@ -16,4 +16,25 @@ then
 		rm wp-config.php.bak
 		wp core update
 	fi
+
+	if [ ! -d /srv/www/regno ]
+	then
+		echo "Downloading regno plugins"
+		git clone https://github.com/blgrgjno/main-blog-network.git /srv/www/regno
+		echo "Copy plugins"
+		cp -R /srv/www/regno/plugins/* /srv/www/wordpress-default/wp-content/plugins
+		cp -R /srv/www/regno/mu-plugins /srv/www/wordpress-default/wp-content/
+		echo "Copy themes"
+		cp -R /srv/www/regno/themes/*  /srv/www/wordpress-default/wp-content/themes
+	else
+		echo "Updating regno plugins (only updating modified, delete /srv/www/regno on hos to overwrite)"
+		cd /srv/www/regno 
+		git pull --rebase origin master
+		echo "Update plugins"
+		cp -Ru /srv/www/regno/plugins/* /srv/www/wordpress-default/wp-content/plugins
+		cp -Ru /srv/www/regno/mu-plugins /srv/www/wordpress-default/wp-content/
+		echo "Update themes"
+		cp -Ru /srv/www/regno/themes/* /srv/www/wordpress-default/wp-content/themes
+	fi
+	
 fi
